b89703d13e60e4eccdd3748c4776461cbaff5f02
rehanafzal1100@gmail.com <rehanafzal1100@gmail.com>
2023-02-13 15:31:15 +0500

    testing commit

--- src/main/java/CompareTwoCSV/CompareCSV.java
+++ src/main/java/CompareTwoCSV/CompareCSV.java
@@ -19,6 +19,8 @@ public class CompareCSV {
     public static void main(String args[]) throws FileNotFoundException, IOException
     {
         SparkContext sparkContxt = new SparkContext(new SparkConf());
+
+        //That's changes store in development branch
         SQLContext spark = new HiveContext(sparkContxt);
         spark.sql("SET spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict");
         spark.sql("SET spark.hadoop.hive.exec.dynamic.partition=true");
