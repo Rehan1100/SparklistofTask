90265e9bb890613f1a78c17936e06b257e896359
rehanafzal1100@gmail.com <rehanafzal1100@gmail.com>
2023-02-13 15:12:24 +0500

    first commit

---
+++ src/main/java/CompareTwoCSV/CompareCSV.java
@@ -0,0 +1,48 @@
+package CompareTwoCSV;
+
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.hive.HiveContext;
+
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class CompareCSV {
+
+
+    public static void main(String args[]) throws FileNotFoundException, IOException
+    {
+        SparkContext sparkContxt = new SparkContext(new SparkConf());
+        SQLContext spark = new HiveContext(sparkContxt);
+        spark.sql("SET spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict");
+        spark.sql("SET spark.hadoop.hive.exec.dynamic.partition=true");
+        spark.sql("SET spark.hadoop.hive.ql.exec.UDF=true");
+
+
+
+
+        System.out.print("Exiting spark session");
+        // Table name Extract from Model
+
+
+        Dataset<Row> dataFrameReadngFromIntegrationKuduTable = spark.read()
+                    .option("kudu.master","10.15.55.22:7051")
+                .option("kudu.table",  "tfcsemantic" + "." + "recyclecapacityoperationcentersreport")
+                .format("kudu").load();
+
+
+        System.out.print("dataFrameReadngFromIntegrationKuduTable");
+
+        dataFrameReadngFromIntegrationKuduTable.createOrReplaceTempView("dataFrameReadngFromIntegrationKuduTable");
+
+        dataFrameReadngFromIntegrationKuduTable.show();
+
+
+    }
+}
